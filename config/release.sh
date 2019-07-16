d138e3952b5d43830abde39929f54b6e6f51abd6
Milan Sekulic <work.milansekulic@gmail.com>
2019-07-16 14:03:54 +0200

    Bump version to 0.1.9-RC1

--- config/release.sh
+++ config/release.sh
@@ -6,9 +6,9 @@ git config user.name "Milan Sekulic"
 git config user.email "work.milansekulic@gmail.com"
 git checkout -b release/${branch} HEAD
 git checkout release/${branch}
-./gradlew app:incrementBuildNumber
+./gradlew app:incrementPatchNumber
 git add --all
 git commit -m "Bump version to $version"
-git push -u origin release/${branch}
+git push -u origin release/${branch} -f
 
 
